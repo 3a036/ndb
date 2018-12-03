7dbed117fe1b5b0a2704f7d31d39e19786d8beda
yejinshou <329365307@qq.com>
2018-12-03 16:07:32 +0800

    add must* func

--- engine.go
+++ engine.go
@@ -1,5 +1,10 @@
 package ndb
 
+import (
+	"fmt"
+	"reflect"
+)
+
 const DefaultDBName = "default"
 
 var (
@@ -24,8 +29,32 @@ func Get(row Row) Row {
 	return DefaultDB.Get(row)
 }
 
+func MustGet(cond Row) Row {
+	row := DefaultDB.Get(cond)
+	if row == nil {
+		panic(fmt.Errorf("MustGet fail %+v", cond))
+	}
+	return row
+}
+
 func GetByIndex(row Row, indexName string) []int {
 	return DefaultDB.GetByIndex(row, indexName)
+}
+
+func MustFirst(cond Row, indexName string) Row {
+	ridArr := DefaultDB.GetByIndex(cond, indexName)
+	if len(ridArr) > 1 {
+		panic(fmt.Errorf("MustFirst %s fail. %d", indexName, len(ridArr)))
+	}
+
+	if len(ridArr) == 0 {
+		return nil
+	}
+
+	rid := ridArr[0]
+	val := reflect.ValueOf(cond).Elem()
+	val.FieldByName("Rid").Set(reflect.ValueOf(rid))
+	return MustGet(cond)
 }
 
 func GetStat(row Row, statName string, all bool) []*Stat {
@@ -40,20 +69,51 @@ func UpdateField(row Row, fieldName string, cmd string, value interface{}, stric
 	return DefaultDB.UpdateFiled(row, fieldName, cmd, value, strict)
 }
 
+func MustUpdateField(cond Row, fieldName string, cmd string, value interface{}) {
+	_, _, err := UpdateField(cond, fieldName, cmd, value, false)
+	if err != nil {
+		panic(fmt.Errorf("MustUpdateField fail %+v %s %s %+v %s", cond, fieldName, cmd, value, err.Error()))
+	}
+}
+
 func Insert(row Row) error {
 	return DefaultDB.Insert(row)
+}
+
+func MustInsert(cond Row) {
+	if err := Insert(cond); err != nil {
+		panic(fmt.Errorf("MustInsert fail %+v %+v", cond, err))
+	}
 }
 
 func Load(row Row) error {
 	return DefaultDB.Load(row)
 }
 
+func MustLoad(cond Row) {
+	if err := Load(cond); err != nil {
+		panic(fmt.Errorf("MustLoad fail %+v %+v", cond, err))
+	}
+}
+
 func Update(row Row) error {
 	return DefaultDB.Update(row)
 }
 
+func MustUpdate(cond Row) {
+	if err := Update(cond); err != nil {
+		panic(fmt.Errorf("MustUpdate fail %+v %+v", cond, err))
+	}
+}
+
 func UpdateFunc(row Row, cb func(row Row) bool) error {
 	return DefaultDB.UpdateFunc(row, cb)
+}
+
+func MustUpdateFunc(cond Row, cb func(row Row) bool) {
+	if err := UpdateFunc(cond, cb); err != nil {
+		panic(fmt.Errorf("MustUpdateFunc fail %+v %+v", cond, err))
+	}
 }
 
 func GetTable(tableName string) *Table {
